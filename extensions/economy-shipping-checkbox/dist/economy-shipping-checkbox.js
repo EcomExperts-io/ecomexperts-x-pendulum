116f17ce16be43584e758f888c4b4bce70e15053
Sadia-Ecom-Experts <sadia.bibi@ecomexperts.io>
2023-12-05 20:08:09 +0400

    [FEATURE]: added checkbox along with validation for international economy shipping consent

--- extensions/economy-shipping-checkbox/dist/economy-shipping-checkbox.js
+++ extensions/economy-shipping-checkbox/dist/economy-shipping-checkbox.js
@@ -19547,41 +19547,42 @@ ${errorInfo.componentStack}`);
   );
   function Extension() {
     const deliveryGroups = useDeliveryGroups();
-    const canBlockProgress = useExtensionCapability("block_progress");
     const SelectedDelivery = deliveryGroups[0].deliveryOptions.filter(({ handle }) => handle == deliveryGroups[0].selectedDeliveryOption.handle);
-    console.log(SelectedDelivery);
-    console.log(SelectedDelivery[0].title === "International Economy" ? true : false);
     const [error, setError] = (0, import_react12.useState)("");
-    useBuyerJourneyIntercept(
-      ({ canBlockProgress: canBlockProgress2 }) => {
-        return canBlockProgress2 ? {
+    const [isChecked, setChecked] = (0, import_react12.useState)(false);
+    const errorText = "To continue with your purchase, agree to the international shipping terms.";
+    const canBlockProgress = useExtensionCapability("block_progress");
+    useBuyerJourneyIntercept(({ canBlockProgress: canBlockProgress2 }) => {
+      if (canBlockProgress2 && !isChecked) {
+        return {
           behavior: "block",
-          reason: "Invalid shipping country",
-          errors: [
-            {
-              message: "Sorry, we can only ship to Canada",
-              // Show an error underneath the country code field
-              target: "$.cart.deliveryGroups[0].deliveryAddress.countryCode"
-            },
-            {
-              // In addition, show an error at the page level
-              message: "Please use a different address."
+          reason: errorText,
+          perform: (result) => {
+            if (result.behavior === "block") {
+              setError(errorText);
             }
-          ]
-        } : {
-          behavior: "allow"
+          }
         };
       }
-    );
+      return {
+        behavior: "allow",
+        perform: () => {
+          setError("");
+        }
+      };
+    });
+    const handleChange = (value) => {
+      setChecked((prev) => !prev);
+      value ? setError("") : setError(errorText);
+    };
     return SelectedDelivery[0].title === "International Economy" ? /* @__PURE__ */ (0, import_jsx_runtime4.jsx)(
       Checkbox2,
       {
         id: "shipping-consent",
         name: "ShippingConsent",
         error,
-        onChange: (value) => {
-          value ? setError("") : setError("To continue with your purchase, agree to the international shipping terms.");
-        },
+        required: canBlockProgress,
+        onChange: handleChange,
         children: "I understand that this item is non-refundable, that the customer is responsible for any customs fees and clearance, and that Pendulum will not be able to track the package once it departs from the U.S."
       }
     ) : null;
--- extensions/economy-shipping-checkbox/src/Checkout.jsx
+++ extensions/economy-shipping-checkbox/src/Checkout.jsx
@@ -16,49 +16,52 @@ export default reactExtension(
 function Extension() {
 
   const deliveryGroups = useDeliveryGroups()
- 
-const canBlockProgress = useExtensionCapability("block_progress");
   const SelectedDelivery = deliveryGroups[0].deliveryOptions.filter(({handle})=>handle == deliveryGroups[0].selectedDeliveryOption.handle)
-  console.log(SelectedDelivery)
-  console.log( SelectedDelivery[0].title === "International Economy" ? true : false)
   const [error, setError] = useState("")
+  const [isChecked, setChecked] = useState(false)
 
-    useBuyerJourneyIntercept(
-    ({canBlockProgress}) => {
-      return canBlockProgress 
-        ? {
-            behavior: 'block',
-            reason: 'Invalid shipping country',
-            errors: [
-              {
-                message:
-                  'Sorry, we can only ship to Canada',
-                // Show an error underneath the country code field
-                target:
-                  '$.cart.deliveryGroups[0].deliveryAddress.countryCode',
-              },
-              {
-                // In addition, show an error at the page level
-                message:
-                  'Please use a different address.',
-              },
-            ],
+  const errorText = "To continue with your purchase, agree to the international shipping terms."
+
+  
+  const canBlockProgress= useExtensionCapability("block_progress");
+   // Use the `buyerJourney` intercept to conditionally block checkout progress
+   useBuyerJourneyIntercept(({ canBlockProgress }) => {
+    // Validate that the age of the buyer is known, and that they're old enough to complete the purchase
+    if (canBlockProgress && !isChecked) {
+      return {
+        behavior: "block",
+        reason: errorText,
+        perform: (result) => {
+          if (result.behavior === "block") {
+            setError(errorText);
           }
-        : {
-            behavior: 'allow',
-          };
-    },
-  );
- 
+        },
+      };
+    }
+
+    return {
+      behavior: "allow",
+      perform: () => {
+        setError("")
+      },
+    };
+  });
+
+   
+ const handleChange = (value)=>{
+  setChecked((prev)=>!prev)
+  value ? setError("") : setError(errorText)
+ }
+
+
   return (
     SelectedDelivery[0].title === "International Economy" ?
     <Checkbox 
     id="shipping-consent" 
     name="ShippingConsent"
     error={error}
-    onChange={(value)=>{
-      value ? setError("") : setError("To continue with your purchase, agree to the international shipping terms.")
-    }}  
+    required={canBlockProgress}
+    onChange={handleChange}  
     >
       I understand that this item is non-refundable, that the customer is responsible for any customs fees and clearance, and that Pendulum will not be able to track the package once it departs from the U.S.    
     </Checkbox> :
