ece9a888cd81b67ebe83cefe08353c9f990af249
Shan Akbar <shan.akbar@ecomexperts.io>
2023-12-15 20:37:52 +0500

    [Feature]: Adds Thank you page notification

---
+++ extensions/thankyou-page-notification/src/Checkout.jsx
@@ -0,0 +1,22 @@
+import {
+  reactExtension,
+  Banner
+} from '@shopify/ui-extensions-react/checkout';
+
+const thankYouPage = reactExtension(
+  'purchase.thank-you.block.render',
+  () => <Extension />,
+);
+
+export { thankYouPage };
+
+function Extension() {
+  const notificationText = "We are transitioning to a new operations system, so apologies if you receive duplicative shipment notifications from the team. You can reach out to our Customer Care team any time if there is any confusion.";
+
+  return (
+    <Banner
+    status="critical"
+    title={notificationText}
+    />
+  );
+}
